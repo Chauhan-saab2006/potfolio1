62f0076cbf5fa2b3e808e819600148dc4240587c
ashwani-chauhan2006 <ac6845578@gmail.com>
2025-09-05 16:19:01 +0530

    add section designs

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 function showSection(id) {
-  // hide all sections
-  document.querySelectorAll('.section').forEach(sec => {
-    sec.classList.remove('active');
-    sec.style.left = "-100vw"; // move back off-screen
-  });
-
-  // show selected section
-  document.getElementById(id).classList.add('active');
-}
+      document.querySelectorAll('.section').forEach(sec => {
+        sec.classList.remove('active');
+      });
+      document.getElementById(id).classList.add('active');
+    }
